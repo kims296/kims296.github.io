f299fdd7b5127f345d4e836a5582b8d2a135a39f
Sarah Kim <kims296@newschool.edu>
2016-11-16 19:10:29 +0000

    anything

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -13,11 +13,12 @@ var myCounter = 1;
 
 $(".dot").each(function() {
     $(this).css("opacity", opacity/150); 
-    $(this).html(myCounter); myCounter++;
+    $(this).html(myCounter); 
+    myCounter++;
     $(this).css("height", myCounter+50);
     $(this).css("width", myCounter+50);
-    $(this).css("top",Math.floor(Math.random()*$
-    (window).height()));
+    console.log(myCounter+50);
+    $(this).css("top",Math.floor(Math.random()*$(window).height()));
     
     myCounter++;
     opacity++;
