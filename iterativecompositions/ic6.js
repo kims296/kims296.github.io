2198cf23b88f0af9ff45e138087f94e48e27dc28
Sarah Kim <kims296@newschool.edu>
2016-11-21 15:28:18 +0000

    anything

---
+++ iterativecompositions/ic6.js
@@ -0,0 +1,35 @@
+
+
+for (var i=0; i < 100; i++) {
+    $("body").append("<div class='dot'></div>");
+    
+}
+
+var opacity = 0;
+var myCounter = 1;
+
+
+ 
+
+$(".dot").each(function() {
+    $(this).css("opacity", opacity/100); 
+    $(this).html(myCounter); 
+    myCounter++;
+    $(this).css("height", myCounter+50);
+    $(this).css("width", myCounter+50);
+    console.log(myCounter+50);
+    $(this).css("top",Math.floor(Math.random()*$(window).height()));
+    
+    myCounter++;
+    opacity++;
+});
+
+$(".dot").click(function() {
+    $(this).css("background-color", "gray");
+    $(this).toggleClass("whatever");
+});
+
+$(".dot").click(function() {
+    $(this).css("background-color" , "white");
+    $(this).toggleClass("point");
+});
