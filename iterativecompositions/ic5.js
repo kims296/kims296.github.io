a89b603d80a3a888a3363adebbc4a564674cf649
Sarah Kim <kims296@newschool.edu>
2016-12-14 08:10:52 +0000

    anything

--- iterativecompositions/ic5.js
+++ iterativecompositions/ic5.js
@@ -1,6 +1,8 @@
-for (var i=0; i < 500; i++) {
+$("body").css("background-color" ,"gray");
+
+for (var i=0; i < 100; i++) {
     $("body").append("<div class='dot'></div>");
-  
+    $("body").append("<img src='img/cotton.jpg'></img>");
 }
 
 var opacity = 0;
@@ -10,24 +12,21 @@ var myCounter = 1;
  
 
 $(".dot").each(function() {
-    $(this).css("opacity", opacity/0); 
+    $(this).css("opacity", opacity/100); 
     $(this).html(myCounter); 
+    myCounter++;
     $(this).css("height", myCounter+50);
     $(this).css("width", myCounter+50);
     console.log(myCounter+50);
     $(this).css("top",Math.floor(Math.random()*$(window).height()));
     
-
+    myCounter++;
     opacity++;
 });
 
 $(".dot").click(function() {
-    $(this).css("background-color", "gray");
+    $(this).css("background-color", "baby-blue");
+    
     $(this).toggleClass("whatever");
-});
-
-$(".dot").click(function() {
-    $(this).css("background-color" , "white");
-    $(this).toggleClass("point");
 });
 
