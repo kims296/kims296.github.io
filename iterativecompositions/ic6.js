c486cb4f266255f9d275743f6ddd242dcd47564d
Sarah Kim <kims296@newschool.edu>
2016-12-14 17:06:13 +0000

    anything

--- iterativecompositions/ic6.js
+++ iterativecompositions/ic6.js
@@ -11,7 +11,6 @@ var myCounter = 1;
 
 $(".dot").each(function() {
     $(this).css("opacity", opacity/0); 
-    $(this).html(myCounter); 
     $(this).css("height", myCounter+50);
     $(this).css("width", myCounter+50);
     console.log(myCounter+50);
