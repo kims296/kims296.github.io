b6677084dacdf2e2486cae5d8098bd0d63a6ebe4
kims296 <sarahekim@Sarahs-MacBookPro.local>
2017-02-22 10:15:57 -0500

    commit

---
+++ ci-2017/javascript/java.js
@@ -0,0 +1,9 @@
+console.log("it works!");
+
+function multiply_by_three(thing_to_multiply) {
+	var final_result = thing_to_multiply + 3;
+
+	var output = "i got: " + thing_to_multiply + "and the result is ";
+
+	console.log(output + final_result);
+}
