bb7d9f014fa2f13727cdca23875597b78519215b
kims296 <sarahekim@Sarahs-MacBookPro.local>
2017-04-19 11:08:33 -0400

    commit

---
+++ ci-2017/final/collection.js
@@ -0,0 +1,42 @@
+var days = [
+
+{
+	"daynumber" : 1,
+	    "videos" : [
+		{
+		"time" : "morning",
+		"source": "filename.mp4",
+		"feels": ["happy", "frustrated"]
+		},
+		{
+		"time" : "afternoon",
+		"source": "filename2.mp4",
+		"feels": ["sad", "frustrated"]
+		}
+	]
+},
+{
+	"daynumber" : 2,
+		"videos" : [
+		{
+		"time" : "morning",
+		"source": "filename.mp4",
+		"feels": ["happy", "frustrated"]
+		}
+	]
+},
+{
+	"daynumber" : 3,
+		"videos" : [
+		{
+		"time" : "morning",
+		"source": "filename.mp4",
+		"feels": ["happy", "frustrated"]
+		}
+	]
+},
+{
+	"daynumber" : 4,
+}
+
+];
