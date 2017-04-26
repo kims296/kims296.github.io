2547f5251f0eee0fc869010b61d394bd13c5d4e9
kims296 <sarahekim@Sarahs-MacBookPro.local>
2017-04-26 11:33:34 -0400

    commit

--- ci-2017/final/collection.js
+++ ci-2017/final/collection.js
@@ -37,6 +37,122 @@ var days = [
 },
 {
 	"daynumber" : 4,
-}
-
+		"videos" : [
+		{
+		"time" : "morning",
+		"source": "filename.mp4",
+		"feels": ["happy", "frustrated"]
+		}
+	]
+},
+{
+	"daynumber" : 5,
+		"videos" : [
+		{
+		"time" : "morning",
+		"source": "filename.mp4",
+		"feels": ["happy", "frustrated"]
+		}
+	]
+},
+{
+	"daynumber" : 6,
+		"videos" : [
+		{
+		"time" : "morning",
+		"source": "filename.mp4",
+		"feels": ["happy", "frustrated"]
+		}
+	]
+},
+{
+	"daynumber" : 7,
+		"videos" : [
+		{
+		"time" : "morning",
+		"source": "filename.mp4",
+		"feels": ["happy", "frustrated"]
+		}
+	]
+},
+{
+	"daynumber" : 8,
+		"videos" : [
+		{
+		"time" : "morning",
+		"source": "filename.mp4",
+		"feels": ["happy", "frustrated"]
+		}
+	]
+},
+{
+	"daynumber" : 9,
+		"videos" : [
+		{
+		"time" : "morning",
+		"source": "filename.mp4",
+		"feels": ["happy", "frustrated"]
+		}
+	]
+},
+{
+	"daynumber" : 10,
+		"videos" : [
+		{
+		"time" : "morning",
+		"source": "filename.mp4",
+		"feels": ["happy", "frustrated"]
+		}
+	]
+},
+{
+	"daynumber" : 11,
+		"videos" : [
+		{
+		"time" : "morning",
+		"source": "filename.mp4",
+		"feels": ["happy", "frustrated"]
+		}
+	]
+},
+{
+	"daynumber" : 12,
+		"videos" : [
+		{
+		"time" : "morning",
+		"source": "filename.mp4",
+		"feels": ["happy", "frustrated"]
+		}
+	]
+},
+{
+	"daynumber" : 13,
+		"videos" : [
+		{
+		"time" : "morning",
+		"source": "filename.mp4",
+		"feels": ["happy", "frustrated"]
+		}
+	]
+},
+{
+	"daynumber" : 14,
+		"videos" : [
+		{
+		"time" : "morning",
+		"source": "filename.mp4",
+		"feels": ["happy", "frustrated"]
+		}
+	]
+},
+{
+	"daynumber" : 15,
+		"videos" : [
+		{
+		"time" : "morning",
+		"source": "filename.mp4",
+		"feels": ["happy", "frustrated"]
+		}
+	]
+},
 ];
