84c160266050a6a327dd3aeab2e99e731abbf0a0
kims296 <sarahekim@Sarahs-MacBookPro.local>
2017-05-09 21:17:45 -0400

    commit

--- ci-2017/final/main.js
+++ ci-2017/final/main.js
@@ -1,10 +1,48 @@
-var container = document.queryselector(".wrapper");
+var container = document.querySelector(".wrapper");
 
-if (container.id = "eating") {
+if (container.id == "eating") {
 	_(days).filter( function(thisday) {
-		if _(thisday.feels).contains("eating")  {
+		if (_(thisday.feels).contains("eating"))  {
 			return true;
 		}
 		else return false;
 	}  )
-}
+} if else (container.id = "happy") {
+	_(days).filter( function(thisday) {
+		if (_(thisday.feels).contains("happy"))  {
+			return true;
+		}
+		else return false;
+	}  )
+
+} if else (container.id = "normal") {
+	_(days).filter( function(thisday) {
+		if (_(thisday.feels).contains("normal"))  {
+			return true;
+		}
+		else return false;
+	}  )
+
+	} if else (container.id = "partying") {
+	_(days).filter( function(thisday) {
+		if (_(thisday.feels).contains("partying"))  {
+			return true;
+		}
+		else return false;
+	}  )
+
+} if else (container.id = "tired") {
+	_(days).filter( function(thisday) {
+		if (_(thisday.feels).contains("tired"))  {
+			return true;
+		}
+		else return false;
+	}  )
+
+} if else (container.id = "weird") {
+	_(days).filter( function(thisday) {
+		if (_(thisday.feels).contains("weird"))  {
+			return true;
+		}
+		else return false;
+	}  )
