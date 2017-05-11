71105381f7b97064d2da0be5ed8bbd742e16e7b1
kims296 <sarahekim@Sarahs-MacBookPro.local>
2017-05-11 15:11:43 -0400

    commit

---
+++ ci-2017/final/time/evening.js
@@ -0,0 +1,170 @@
+var container = document.createElement("div");
+container.classList.add("container");
+var body = document.querySelector("body");
+
+
+
+function day1() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+video.src = "../videos/day1.mp4";
+}
+function day3() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day3.mp4";
+}
+function day5() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day5.mp4";
+}
+function day6() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day6.mp4";
+}
+
+function day7() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day7.mp4";
+}
+
+function day9() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day9.mp4";
+}
+function day10() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day10.mp4";
+}
+function day11() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day11.mp4";
+}
+function day14() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day14.mp4";
+}
+function day15() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day15.mp4";
+}
+function day17() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day17.mp4";
+}
+function day18() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day18.mp4";
+}
+function day20() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day20.mp4";
+}
+function day21() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day21.mp4";
+
+}
+
+function day24() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day24.mp4";
+}
+function day25() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day25.mp4";
+}
+function day27() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day27.mp4";
+}
+function day28() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day28.mp4";
+}
+function day29() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day29.mp4";
+}
+function day30() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day30.mp4";
+}
---
+++ ci-2017/final/time/morning.js
@@ -0,0 +1,81 @@
+var container = document.createElement("div");
+container.classList.add("container");
+var body = document.querySelector("body");
+
+
+
+function day2() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+video.src = "../videos/day2.mp4";
+}
+function day8() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day8.mp4";
+}
+
+function day9() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day9.mp4";
+}
+
+function day12() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day12.mp4";
+}
+function day16() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day19.mp4";
+}
+function day19() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day19.mp4";
+}
+function day23() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day23.mp4";
+}
+function day24() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day24.mp4";
+}
+function day26() {
+	console.log("change");
+	//var vid = document.getElementById('video');
+	//vid.src = "";
+	//$("#myVideoTag").attr("src", "../videos/day1.mp4"​​​​)​;
+	var video = document.getElementById('myVideoTag');
+	video.src = "../videos/day26.mp4";
+
+}
